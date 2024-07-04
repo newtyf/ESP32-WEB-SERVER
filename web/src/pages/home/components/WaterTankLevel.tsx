01257c99e5accc5c2025c05d433dbe03f336399b
newt <axelskam.123@outlook.es>
2024-07-04 10:48:27 -0500

    feat(Frontend): :lipstick: Change ml to % for level of water

--- web/src/pages/home/components/WaterTankLevel.tsx
+++ web/src/pages/home/components/WaterTankLevel.tsx
@@ -8,16 +8,16 @@ import {
 import { BadgeCheck, TriangleAlert } from "lucide-react";
 
 interface Props {
-  waterTankLevel: number
+  waterTankLevel: number;
 }
 
-export const WaterTankLevel = ({waterTankLevel}: Props) => {
+export const WaterTankLevel = ({ waterTankLevel }: Props) => {
   return (
     <Card className='min-h-96 h-full flex flex-col'>
       <CardHeader className='px-3 py-2'>
         <CardTitle className='scroll-m-20 text-base font-extrabold tracking-tight flex justify-between items-center'>
           Nivel de agua del tanque
-          {waterTankLevel <= 500 && (
+          {waterTankLevel <= 25 && (
             <TooltipProvider>
               <Tooltip>
                 <TooltipTrigger>
@@ -29,7 +29,7 @@ export const WaterTankLevel = ({waterTankLevel}: Props) => {
               </Tooltip>
             </TooltipProvider>
           )}
-          {500 < waterTankLevel && waterTankLevel < 1000 && (
+          {25 < waterTankLevel && waterTankLevel <= 100 && (
             <TooltipProvider>
               <Tooltip>
                 <TooltipTrigger>
@@ -45,44 +45,46 @@ export const WaterTankLevel = ({waterTankLevel}: Props) => {
       </CardHeader>
       <CardContent className='px-3 py-2 flex-1 flex flex-col'>
         <h4 className='scroll-m-20 text-sm tracking-tight'>
-          Cantidad de Agua Actual:{" "}
-          <span className='font-bold'>{waterTankLevel}ml</span>
+          Cantidad de Agua Actual:
+          <span className='font-bold'>{waterTankLevel}%</span>
         </h4>
         <div className='border border-t-0 border-dashed border-gray-500 flex-1  relative mt-8 rounded-lg'>
           <div
             className={`absolute bottom-0 bg-sky-400 w-full rounded-lg`}
-            style={{ height: waterTankLevel / 20 + "%" }}
+            style={{
+              height: (waterTankLevel > 100 ? 100 : waterTankLevel) + "%",
+            }}
           ></div>
           <div className='absolute w-full h-full'>
             <div
               className='absolute border-b border-dashed border-gray-500 w-full text-right'
               style={{ bottom: "0%" }}
             >
-              <span className='pr-2'>0 ml</span>
+              <span className='pr-2'>0%</span>
             </div>
             <div
               className='absolute border-b border-dashed border-gray-500 w-full text-right'
               style={{ bottom: "25%" }}
             >
-              <span className='pr-2'>500 ml</span>
+              <span className='pr-2'>25%</span>
             </div>
             <div
               className='absolute border-b border-dashed border-gray-500 w-full text-right'
               style={{ bottom: "50%" }}
             >
-              <span className='pr-2'>1000 ml</span>
+              <span className='pr-2'>50%</span>
             </div>
             <div
               className='absolute border-b border-dashed border-gray-500 w-full text-right'
               style={{ bottom: "75%" }}
             >
-              <span className='pr-2'>1500 ml</span>
+              <span className='pr-2'>75%</span>
             </div>
             <div
               className='absolute border-b border-dashed border-gray-500 w-full text-right'
               style={{ bottom: "100%" }}
             >
-              <span className='pr-2'>2000 ml</span>
+              <span className='pr-2'>100%</span>
             </div>
           </div>
         </div>
